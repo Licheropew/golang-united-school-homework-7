2e58389cd0c7c6bb01a32aa0469b344a0f43a143
unknown <anton.aksiutsin@gmail.com>
2022-06-03 07:56:29 +0300

    Initial commit with Len, Less, Swap

--- students_test.go
+++ students_test.go
@@ -1,7 +1,10 @@
 package coverage
 
 import (
+	"log"
 	"os"
+	"testing"
+	"time"
 )
 
 // DO NOT EDIT THIS FUNCTION
@@ -17,4 +20,127 @@ func init() {
 }
 
 // WRITE YOUR CODE BELOW
+func TestMain(m *testing.M) {
+	log.Println("START TESTING")
+	code := m.Run()
+	log.Println("TEST END")
+	os.Exit(code)
+}
 
+func TestLenPeople(t *testing.T) {
+	tests := map[string]struct{
+		input People
+		output int
+	}{
+		"Empty": {People{}, 0},
+		"One": {People{
+			{firstName: "Anton", lastName: "A"},
+			}, 1},
+		"Two": {People{
+			{firstName: "Anton", lastName: "A"}, 
+			{firstName: "Anton", lastName: "B"},
+			}, 2},
+		"Three": {People{
+			{firstName: "Anton", lastName: "A"}, 
+			{firstName: "Anton", lastName: "B"}, 
+			{firstName: "Anton", lastName: "C"},
+			}, 3},
+	}
+	for testName, testCase := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := testCase.input.Len()
+			if res != testCase.output {
+				t.Errorf("got %d, expect %d", res, testCase.output)
+			}
+		})
+	}
+}
+
+func TestLessPeople(t *testing.T) {
+	birthday := time.Date(1990, 3, 22, 0, 0, 0, 0, time.UTC)
+	tests := map[string]struct{
+		input People
+		output bool
+		i, j int
+	}{
+		"Birthday and first name even, last name i < j": {People{
+			{firstName: "Anton", lastName: "A", birthDay: birthday}, 
+			{firstName: "Anton", lastName: "B", birthDay: birthday},
+			}, true, 0, 1},
+		"Birthday and first name even, last name i > j": {People{
+			{firstName: "Anton", lastName: "A", birthDay: birthday}, 
+			{firstName: "Anton", lastName: "B", birthDay: birthday},
+			}, false, 1, 0},
+		"Birthday, first and last name even (i==j)": {People{
+			{firstName: "Anton", lastName: "A", birthDay: birthday}, 
+			}, false, 0, 0},
+		"Birthday even, first name i < j": {People{
+			{firstName: "Anton", birthDay: birthday}, 
+			{firstName: "Notna", birthDay: birthday},
+			}, true, 0, 1},
+		"Birthday even, first name i > j": {People{
+			{firstName: "Anton", birthDay: birthday}, 
+			{firstName: "Notna", birthDay: birthday},
+			}, false, 1, 0},
+		"Birthday i > j": {People{
+			{firstName: "Anton", birthDay: birthday.AddDate(0, 1, 0)}, 
+			{firstName: "Notna", birthDay: birthday},
+			}, true, 0, 1},
+		"Birthday i < j": {People{
+			{firstName: "Anton", birthDay: birthday.AddDate(0, 1, 0)}, 
+			{firstName: "Notna", birthDay: birthday},
+			}, false, 1, 0},
+	}
+	for testName, testCase := range tests {
+		t.Run(testName, func(t *testing.T) {
+			res := testCase.input.Less(testCase.i, testCase.j)
+			if res != testCase.output {
+				t.Errorf("got %v, expect %v", res, testCase.output)
+			}
+		})
+	}
+}
+
+func TestSwapFunc(t *testing.T) {
+	tests := map[string]struct{
+		input People
+		output People
+		i, j int
+	}{
+		"Positive case": {
+			People{
+				{firstName: "Anton", lastName: "A"}, 
+				{firstName: "Anton", lastName: "B"},
+			},
+			People{
+				{firstName: "Anton", lastName: "B"}, 
+				{firstName: "Anton", lastName: "A"},
+			}, 0, 1},
+		"Case where i==j": {
+			People{
+				{firstName: "Anton", lastName: "A"}, 
+				{firstName: "Anton", lastName: "B"},
+			},
+			People{
+				{firstName: "Anton", lastName: "A"}, 
+				{firstName: "Anton", lastName: "B"},
+			}, 0, 0},
+	}
+	for testName, testCase := range tests {
+		t.Run(testName, func(t *testing.T) {
+			testCase.input.Swap(testCase.i, testCase.j)
+			if testCase.i != testCase.j {
+				switch {
+				case testCase.input[testCase.i] == testCase.output[testCase.j]:
+					t.Errorf("swap func goes wrong, expect %v, got %v", testCase.output[testCase.j], testCase.input[testCase.i])
+				case testCase.input[testCase.j] == testCase.output[testCase.i]:
+					t.Errorf("swap func goes wrong, expect %v, got %v", testCase.output[testCase.i], testCase.input[testCase.j])
+				}
+			} else {
+				if testCase.input[testCase.i] != testCase.output[testCase.j] {
+					t.Errorf("swap func goes wrong, expect %v, got %v", testCase.output[testCase.j], testCase.input[testCase.i])
+				}
+			}
+		})
+	}
+}
